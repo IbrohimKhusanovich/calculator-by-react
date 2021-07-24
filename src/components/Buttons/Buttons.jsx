daad6cc55333e202661c501b477f8a2eba79b816
Ibrohim <iyuldashev021@gmail.com>
2021-07-24 14:48:33 +0500

    finish code

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
-import './Buttons.css'
-export default class Buttons extends Component{
- render() {
+import "./Buttons.css";
+export default class Buttons extends Component {
+  render() {
     return (
       <div className='btn-grid'>
-        <button value='+' onClick={this.props.onBtns}>Add</button>
-        <button value='-' onClick={this.props.onBtns}>Substract</button>
-        <button value='/' onClick={this.props.onBtns}>Divide</button>
-        <button value='*' onClick={this.props.onBtns}>Multiply</button>
-        <button value='%' onClick={this.props.onBtns}>Modulus</button>
-        <button value='* Math.PI *' onClick={this.props.onBtns}>PI</button>
+        <button value='+' onClick={this.props.onBtns}>
+          Add
+        </button>
+        <button value='-' onClick={this.props.onBtns}>
+          Substract
+        </button>
+        <button value='/' onClick={this.props.onBtns}>
+          Divide
+        </button>
+        <button value='*' onClick={this.props.onBtns}>
+          Multiply
+        </button>
+        <button value='%' onClick={this.props.onBtns}>
+          Modulus
+        </button>
+        <button value='* Math.PI *' onClick={this.props.onBtns}>
+          PI
+        </button>
+        <button onClick={this.props.onReset}>Reset</button>
       </div>
     );
   }
