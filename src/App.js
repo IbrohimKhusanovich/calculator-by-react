6a369387542ca0e15319cc7674ba22a8b316ed73
Ibrohim <iyuldashev021@gmail.com>
2021-07-24 14:31:06 +0500

    calculator

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Form from './components/Form/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className='body-calculator'>
+    <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Buttons from '../Buttons/Buttons';
+import './Form.css'
+export default class Form extends Component {
+  state={
+   value:'',
+   value2:'',
+   answer:''
+ };
+setInputText1=(e)=>{
+ this.setState({value:e.target.value});
+}
+setInputText2=(e)=>{
+ this.setState({value2:e.target.value});
+}
+handleBtns=(el)=>{
+  let value1=+this.state.value
+  let value2=+this.state.value2
+  let target=el.target.value
+  if(this.state.value==''||this.state.value2=='')
+  alert('Ikkala inputga ham qiymat kiritng')
+  else{
+let res=eval(value1 + target + value2)
+this.setState({answer:res})
+}
+}
+render() {
+  return (
+   <div className='form-block'>
+    <label htmlFor="fnumber">First Number</label>
+    <input value={this.state.value} onChange={this.setInputText1} id='fnumber' type="number" />
+    <label htmlFor="snumber">Second Number</label>
+    <input value={this.state.value2a} onChange={this.setInputText2} id='snumber' type="number" />
+    <label htmlFor="answer">Answer</label>
+    <input value={this.state.answer} id='answer'disabled='true' type="number" />
+    <Buttons
+     onBtns={this.handleBtns}
+    />
+   </div>
+  )
+ }
+}
